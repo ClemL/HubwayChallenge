b226ee8d293d8114db21bb3cb4e9d15c76f67249
Clem <Clem.leung@gmail.com>
2014-05-22 16:05:36 -0400

    test load stations and find min / max distances between them.

---
+++ src/Models/Station.java
@@ -0,0 +1,17 @@
+package Models;
+
+import java.util.Date;
+
+public class Station {
+
+	public int id;
+	public String terminal;
+	public String station,municipality,nb_docks;
+	public Double lat,lng;
+	public Date install_date,last_day;
+	
+	public Station() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
